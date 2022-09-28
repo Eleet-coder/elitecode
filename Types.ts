6acb35c84e4682159264aa31e57b753f0a0e171e
FrozenStove <Yessysutter@gmail.com>
2022-09-27 18:48:43 -0700

    Testing setup

--- Types.ts
+++ Types.ts
@@ -1,11 +1,6 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 
 export type UserController = {
-    signup : (req: Request, res: Response, next: NextFunction) => any,
-    login : (req: Request, res: Response, next: NextFunction) => any,
-    
-    declare module "*.png" {
-        const value: any;
-        export default value;
-    }
-      }
+    signup: (req: Request, res: Response, next: NextFunction) => any,
+    login: (req: Request, res: Response, next: NextFunction) => any,
+}
